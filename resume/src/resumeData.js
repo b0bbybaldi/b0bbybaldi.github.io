fd32b4960c0d34456e2cdb9bb118ae8011f28e4c
b0bbybaldi <roberto.baldizon@silibrain.com>
2019-01-27 23:30:27 -0600

    filling up personal information in resumedata

--- resume/src/resumeData.js
+++ resume/src/resumeData.js
@@ -1,104 +1,130 @@
 let resumeData = {
-  "imagebaseurl":"https://b0bbybaldi.github.io/",
+  "imagebaseurl": "https://b0bbybaldi.github.io/",
   "name": "Roberto Baldizon",
-  "role": "Engineer",
-  "linkedinId":"b0bbybaldi",
+  "role": "CEO & Founder",
+  "linkedinId": "b0bbybaldi",
   "skypeid": "rcbaldizon",
-  "roleDescription": "I like dabbling in various parts of frontend development and like to learn about new technologies, write technical articles or simply play games in my free time.",
-  "socialLinks":[
-      {
-        "name":"linkedin",
-        "url":"https://www.linkedin.com/",
-        "className":"fa fa-linkedin"
-      },
-      {
-        "name":"github",
-        "url":"http://github.com/",
-        "className":"fa fa-github"
-      },
-      {
-        "name":"skype",
-        "url":"http://skype.com",
-        "className":"fa fa-skype"
-      }
-    ],
-  "aboutme":"I have finished my B.E. in Computer Science and have been working in industry for past few years.",
-  "address":"India",
-  "website":"https://www.codeclassifiers.com",
-  "education":[
+  "roleDescription": " Used to getting my hands dirty in the field as a businessman, technology consultant, and engineer experienced in web, software, and energy systems designs to find tailored technology solutions worldwide.",
+  "socialLinks": [
     {
-      "UniversityName":"Mumbai University",
-      "specialization":"Some specialization",
-      "MonthOfPassing":"Jan",
-      "YearOfPassing":"2018",
-      "Achievements":"Some Achievements"
+      "name": "linkedin",
+      "url": "https://www.linkedin.com/in/b0bbybaldi/",
+      "className": "fa fa-linkedin"
     },
     {
-      "UniversityName":"Some University",
-      "specialization":"Some specialization",
-      "MonthOfPassing":"Jan",
-      "YearOfPassing":"2018",
-      "Achievements":"Some Achievements"
+      "name": "github",
+      "url": "https://github.com/b0bbybaldi",
+      "className": "fa fa-github"
+    },
+    {
+      "name": "skype",
+      "url": "https://join.skype.com/invite/EDyWJnTuWvxy",
+      "className": "fa fa-skype"
+    },
+    {
+      "name": "facebook",
+      "url": "www.facebook.com/b0bbybaldi",
+      "className": "fa fa-facebook"
+    },
+    {
+      "name": "instagram",
+      "url": "www.instagram.com/b0bbybaldi",
+      "className": "fa fa-instagram"
+    },
+    {
+      "name": "twitter",
+      "url": "www.twitter.com/b0bbybaldi",
+      "className": "fa fa-twitter"
+    },
+    {
+      "name": "medium",
+      "url": "https://medium.com/@b0bbybaldi",
+      "className": "fa fa-medium"
+    },
+    {
+      "name": "mail",
+      "mailto": "rcbaldizon@gmail.com",
+      "className": "fa fa-mail"
+    }
+
+  ],
+  "aboutme": " I am originally from Guatemala, where I went to high school, but I feel comfortable anywhere in the world and think of it as my home. I speak 3 languages fluently, and one conversationally, not taking into account the programming languages that I also know. I'm passionate about technology, science, and their business applications for generating postive economic and social impact globally.",
+  "address": "San Francisco, CA",
+  "website": "https://b0bbybaldi.github.io",
+  "education": [
+    {
+      "UniversityName": "Mumbai University",
+      "specialization": "Some specialization",
+      "MonthOfPassing": "Jan",
+      "YearOfPassing": "2018",
+      "Achievements": "Some Achievements"
+    },
+    {
+      "UniversityName": "Some University",
+      "specialization": "Some specialization",
+      "MonthOfPassing": "Jan",
+      "YearOfPassing": "2018",
+      "Achievements": "Some Achievements"
     }
   ],
-  "work":[
+  "work": [
     {
-      "CompanyName":"Some Company",
-      "specialization":"Some specialization",
-      "MonthOfLeaving":"Jan",
-      "YearOfLeaving":"2018",
-      "Achievements":"Some Achievements"
+      "CompanyName": "Some Company",
+      "specialization": "Some specialization",
+      "MonthOfLeaving": "Jan",
+      "YearOfLeaving": "2018",
+      "Achievements": "Some Achievements"
     },
     {
-      "CompanyName":"Some Company",
-      "specialization":"Some specialization",
-      "MonthOfLeaving":"Jan",
-      "YearOfLeaving":"2018",
-      "Achievements":"Some Achievements"
+      "CompanyName": "Some Company",
+      "specialization": "Some specialization",
+      "MonthOfLeaving": "Jan",
+      "YearOfLeaving": "2018",
+      "Achievements": "Some Achievements"
     }
   ],
-  "skillsDescription":"Your skills here",
-  "skills":[
+  "skillsDescription": "Your skills here",
+  "skills": [
     {
-      "skillname":"HTML5"
+      "skillname": "HTML5"
     },
     {
-      "skillname":"CSS"
+      "skillname": "CSS"
     },
     {
-      "skillname":"Reactjs"
+      "skillname": "Reactjs"
     }
   ],
-  "portfolio":[
+  "portfolio": [
     {
-      "name":"project1",
-      "description":"mobileapp",
-      "imgurl":"images/portfolio/phone.jpg"
+      "name": "project1",
+      "description": "mobileapp",
+      "imgurl": "images/portfolio/phone.jpg"
     },
     {
-      "name":"project2",
-      "description":"mobileapp",
-      "imgurl":"images/portfolio/project.jpg"
+      "name": "project2",
+      "description": "mobileapp",
+      "imgurl": "images/portfolio/project.jpg"
     },
     {
-      "name":"project3",
-      "description":"mobileapp",
-      "imgurl":"images/portfolio/project2.png"
+      "name": "project3",
+      "description": "mobileapp",
+      "imgurl": "images/portfolio/project2.png"
     },
     {
-      "name":"project4",
-      "description":"mobileapp",
-      "imgurl":"images/portfolio/phone.jpg"
+      "name": "project4",
+      "description": "mobileapp",
+      "imgurl": "images/portfolio/phone.jpg"
     }
   ],
-  "testimonials":[
+  "testimonials": [
     {
-      "description":"This is a sample testimonial",
-      "name":"Some technical guy"
+      "description": "This is a sample testimonial",
+      "name": "Some technical guy"
     },
     {
-      "description":"This is a sample testimonial",
-      "name":"Some technical guy"
+      "description": "This is a sample testimonial",
+      "name": "Some technical guy"
     }
   ]
 }
