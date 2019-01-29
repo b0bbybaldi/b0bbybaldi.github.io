42aa9b935d7a98cae27f5e0f972d79c0784f0532
b0bbybaldi <roberto.baldizon@silibrain.com>
2019-01-29 10:13:20 -0600

    set up updated

---
+++ build/precache-manifest.a3c501eac3ab9b9be2e32b3d1ed277ed.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "100020dd230a9cef2152",
+    "url": "/static/js/main.100020dd.chunk.js"
+  },
+  {
+    "revision": "f739feb7d61c48a46bed",
+    "url": "/static/js/1.f739feb7.chunk.js"
+  },
+  {
+    "revision": "100020dd230a9cef2152",
+    "url": "/static/css/main.94562995.chunk.css"
+  },
+  {
+    "revision": "9f4ed09b201f58733cd4d1daf582b4db",
+    "url": "/index.html"
+  }
+];
