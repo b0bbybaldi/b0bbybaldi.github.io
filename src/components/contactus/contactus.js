0cd110fb46334c94636add658f76ac69a51eed51
b0bbybaldi <roberto.baldizon@silibrain.com>
2019-01-30 00:44:19 -0600

    name change

---
+++ src/components/contactus/contactus.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+export default class ContactUs extends Component {
+  render() {
+    // let resumeData = this.props.resumeData;
+    return (
+      <section id="contact">
+          <div className="row section-head">
+            <div className="ten columns">
+              <p className="lead">
+              Feel free to contact me for any work or suggestions through email or social media.
+              </p>
+            </div>
+          </div>
+          <div className="row">
+            <aside className="eigth columns footer-widgets">
+              <div className="widget">
+                <h3>
+                  <a href="mailto=rcbaldizon@gmail.com">Send Me an Email !</a>
+                </h3>
+              </div>
+            </aside>
+          </div>
+        </section>
+        );
+  }
+}
