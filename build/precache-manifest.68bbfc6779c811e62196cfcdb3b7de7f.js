e3f761ab0f94f8c6ace4562c7aa8559de6a474e7
b0bbybaldi <roberto.baldizon@silibrain.com>
2019-01-29 18:20:18 -0600

    YAML added to reademe

---
+++ build/precache-manifest.68bbfc6779c811e62196cfcdb3b7de7f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3b96116072eb85f5a3a2",
+    "url": "/Portfolio/static/js/runtime~main.3b961160.js"
+  },
+  {
+    "revision": "09e3079236d602e1c5df",
+    "url": "/Portfolio/static/js/main.09e30792.chunk.js"
+  },
+  {
+    "revision": "f739feb7d61c48a46bed",
+    "url": "/Portfolio/static/js/1.f739feb7.chunk.js"
+  },
+  {
+    "revision": "09e3079236d602e1c5df",
+    "url": "/Portfolio/static/css/main.95ada69a.chunk.css"
+  },
+  {
+    "revision": "61bcfd27596a035cd3eaec8661c7cfa2",
+    "url": "/Portfolio/index.html"
+  }
+];
